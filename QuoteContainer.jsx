882066a352a84a79a7156c0dc7830b1edbe94aaf
charlie <charliealonso2001@gmail.com>
2021-10-22 14:13:18 -0500

    newQuote button now generates a new quote

--- QuoteContainer.jsx
+++ QuoteContainer.jsx
@@ -9,9 +9,11 @@ class QuoteContainer extends React.Component {
         error: null,
         isLoaded: false,
         quotes: [],
-        quotesLength: null,
-        // apiQuotes: []
         };
+    }      
+    // new quote button function
+    newQuote = () => {
+      this.setState({error: null}) // apparently just touching the state calls render
     }
 
     componentDidMount() {
@@ -86,54 +88,21 @@ class QuoteContainer extends React.Component {
           : '' // don't print anything if the quote isn't selected
         ))
       )
+
+
       return(
           <div className="quote-container" id="quote-container">
-            
-            {/* {quotes.map((quote, index) => (
-              index === rand ? // sellects a random quote from quotes
-              <div key="index">
-                {/* Quote , print quote's text 
-                <div className="quote-text">
-                    <i className="fas fa-quote-left"></i>
-                    <span id="quote">{ quote.text }</span>
-                </div>
-                {/* Author , prind quote's author 
-                <div className="quote-author">
-                    <span id="author">{ quote.author }</span>
-                </div>
-              </div>
-              : '' // don't print anything if the quote isn't selected
-            ))} */}
 
             { renderQuote() }
 
               {/* Buttons */}
               <div className="button-container">
-                  <button onClick={renderQuote} className="twitter-button" id="twitter" title="Tweet This!">
+                  <button className="twitter-button" id="twitter" title="Tweet This!">
                       <i className="fab fa-twitter"></i>
                   </button>
-                  <button id="new-quote">New Quote</button>
+                  <button onClick={this.newQuote} id="new-quote">New Quote</button>
               </div>
           </div>    
       )
     }
 }
-
-// ReactDOM.render(
-//     <div class="quote-container" id="quote-container">
-//         <div class="quote-text">
-//             <i class="fas fa-quote-left"></i>
-//             <span id="quote">lorem quote</span>
-//         </div>
-//         <div class="quote-author">
-//             <span id="author">lorem author</span>
-//         </div>
-//         <div class="button-container">
-//             <button class="twitter-button" id="twitter" title="Tweet This!">
-//                 <i class="fab fa-twitter"></i>
-//             </button>
-//             <button id="new-quote">New Quote</button>
-//         </div>
-//     </div>,
-//     document.getElementById('root')
-// ) 
